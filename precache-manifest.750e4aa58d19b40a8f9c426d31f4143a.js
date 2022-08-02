2c30045ac0d6214bd202d9c0eea6c950f9fc63f5
EngrKingsley <engrkingsley@yahoo.com>
2022-08-02 01:01:07 +0100

    Updates

---
+++ precache-manifest.750e4aa58d19b40a8f9c426d31f4143a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a9fec4a2a7e99e466a9b655abfb4d36",
+    "url": "/engrkingsley-gh-pages/index.html"
+  },
+  {
+    "revision": "36362d1fd5aa5dfaae8d",
+    "url": "/engrkingsley-gh-pages/static/css/2.8e2610c1.chunk.css"
+  },
+  {
+    "revision": "d8bfdff4d011c37422fc",
+    "url": "/engrkingsley-gh-pages/static/css/main.b3efca0b.chunk.css"
+  },
+  {
+    "revision": "36362d1fd5aa5dfaae8d",
+    "url": "/engrkingsley-gh-pages/static/js/2.a3db1ed1.chunk.js"
+  },
+  {
+    "revision": "cab22c2f853eb971190951a87268c280",
+    "url": "/engrkingsley-gh-pages/static/js/2.a3db1ed1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d8bfdff4d011c37422fc",
+    "url": "/engrkingsley-gh-pages/static/js/main.9a8c9226.chunk.js"
+  },
+  {
+    "revision": "5cd71ae379576f3c4e51",
+    "url": "/engrkingsley-gh-pages/static/js/runtime-main.65cab686.js"
+  }
+]);
